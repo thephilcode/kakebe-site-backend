f96753c27eccea19a2d71cab259961abc4c04dcf
Philip Ayo <philipayolexz@gmail.com>
2025-12-06 19:28:58 +0300

    Add Cloudinary upload provider

---
+++ config/plugins.ts
@@ -0,0 +1,18 @@
+// config/plugins.ts  (or .js – same content)
+export default {
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+};
