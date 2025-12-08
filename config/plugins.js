aaa2bbc8f0ab204b33f976229805346468ec1f60
Philip Ayo <philipayolexz@gmail.com>
2025-12-08 18:17:54 +0300

    Specify upload preset

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,8 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+        upload_preset: env('CLOUDINARY_UPLOAD_PRESET', 'strapi_unsigned'),
+        unsigned: true,
       },
       actionOptions: {
         upload: {},
